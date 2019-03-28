ecd048bffbd4a0d58b2bac1665fd1fe794666c15
ILA\Administrator <ila2002@gmail.com>
2019-03-29 00:01:51 +0800

    DDD

---
+++ run.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+import six
+import packaging
+import packaging.version
+import packaging.specifiers
+import packaging.requirements
+from app import Checkin_App
+app=Checkin_App()
+app.app_start()
